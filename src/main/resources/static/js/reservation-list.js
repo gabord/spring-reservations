602fe8d2daaf3fd72e361b7fbf72511faba1e82e
Danis Gábor <danisg@jusoft.hu>
2017-11-12 22:46:30 +0100

    Datatable added for Reservation list

--- src/main/resources/static/js/reservation-list.js
+++ src/main/resources/static/js/reservation-list.js
@@ -4,65 +4,97 @@
 
 "use strict";
 
-(function() {
-  
-  
+(function () {
 
-$(document).ready(function() {
+  $(document).ready(function () {
+    var table = null;
 
-  function loadAndDisplayListOfReservations() {
+
+    function deleteWithAlert(id) {
+      swal({
+        title: "Are you sure?",
+        text: "Are you 100% sure to delete this reservation?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+        .then((willDelete) => {
+          if (willDelete) {
+            deleteReservationAjax(id);
+          } 
+        });
+    }
     
-    $('.message').hide();
     
-    var reservationListTemplateSource = $("#reservation-list-template").html();      // get the template's html source
-    var reservationListTemplate = Handlebars.compile(reservationListTemplateSource); // initialize Handlebars template
-
-    $.ajax({
-      url : "/api/reservations/",
-      dataType : 'json',
-      async : true, 
-      cache : false,
-      timeout : 5000, 
-
-      data : {},
-      success : function(response) {
-
-        var h = reservationListTemplate(response);     // generate HTML from the object using the template
-        $("#reservation-list").empty();
-        $("#reservation-list").append(h);              // insert the generated HTML into the document
-      },
-      error : function(XMLHttpRequest, textStatus, errorThrown) {
-        console.log("reservation list retrieval failed ... HTTP status code: " + XMLHttpRequest.status + ' message ' + XMLHttpRequest.responseText);
-        $('#system-error').fadeIn();
-      }
-    });
-  }
-  
-  $("body").on('click', '.action', function(e) { 
-    var reservationId = $(this).attr('reservationId');
+    function deleteReservationAjax(id) {
+      $.ajax({
+        url: '/api/reservations/' + encodeURIComponent(id),
+        method: 'DELETE',
+        async: true,
+        cache: false,
+        timeout: 5000,
     
-    $.ajax({
-      url : '/api/reservations/' + encodeURIComponent(reservationId),                    
-      method: 'DELETE',                           
-      async : true,                          
-      cache : false,                         
-      timeout : 5000,                     
+        data: {},
+    
+        success: function (data, statusText, response) {
+          swal("The reservation is successfully deleted.", {
+            icon: "success",
+          });
+          table.ajax.reload();
+        },
+    
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          console.log("reservation request failed ... HTTP status code: " + XMLHttpRequest.status + ' message ' + XMLHttpRequest.responseText);
+          swal(
+            'Oops...',
+            'Error deleting this reservation. (' + XMLHttpRequest.status + ' - ' + XMLHttpRequest.responseText + ')',
+            'error'
+          )
+          $('#action-error').fadeIn();
+        }
+      });
+    }
 
-      data : {},                               
+    function loadAndDisplayListOfReservations() {
+      table = $('#reservation_table').DataTable({
+        "responsive": true,
+        "scrollX": true,        
+        "ajax": {
+          "url": "/api/reservations/",
+          "dataSrc": "content"
+        },
+        "columns": [
+          { "data": "id" },
+          { "data": "resourceId" },
+          { "data": "startDate" },
+          { "data": "endDate" },
+          { "data": "owner" },
+          {
+            "data": "action", searchable: false, sortable: false,
+            render: function (data, type, row) {
+              return '<a href="#" class="delete-reservation" title="Delete this reservation"><i class="fa fa-trash-o"></i></a>'
+            }
+          }
+        ],
+        "columnDefs": [
+          { "className": "dt-center", "targets": "_all" }
+        ],
+      });
 
-      success : function(data, statusText, response) {          
-        loadAndDisplayListOfReservations();
-      },
-                                                
-      error : function(XMLHttpRequest, textStatus, errorThrown) {   
-        console.log("reservation request failed ... HTTP status code: " + XMLHttpRequest.status + ' message ' + XMLHttpRequest.responseText);
-        $('#action-error').fadeIn();
-      }
-    });
+      $('.message').hide();
+    }
+
+    function addTableEventHandlers() {
+      $('#reservation_table tbody').on('click', 'a.delete-reservation', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+        console.log(table.row( $(this).parents('tr') ).data());
+        deleteWithAlert(data['id']);
+      });
+    }
+
+    loadAndDisplayListOfReservations();
+    addTableEventHandlers();
   });
-  
-  loadAndDisplayListOfReservations();
-});
 
 
 })();
